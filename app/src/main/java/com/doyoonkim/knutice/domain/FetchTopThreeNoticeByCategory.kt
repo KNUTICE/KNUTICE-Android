9890c965a73ac15bd46cf20b3db1479f1d47af56
Doyoon Kim <doyoon3312@kakao.com>
2024-09-24 23:20:50 +0900

    [REFATOR] Apply local repository with improved pipeline
      - Apply improved pipeline for received data backed by local repository implemented.
      - Break down one general function into several 'smaller' function based on its assigned job.

--- app/src/main/java/com/doyoonkim/knutice/domain/FetchTopThreeNoticeByCategory.kt
+++ app/src/main/java/com/doyoonkim/knutice/domain/FetchTopThreeNoticeByCategory.kt
@@ -1,7 +1,7 @@
 package com.doyoonkim.knutice.domain
 
-import android.util.Log
-import com.doyoonkim.knutice.model.NoticeRemoteRepository
+import com.doyoonkim.knutice.model.NoticeLocalRepository
+import com.doyoonkim.knutice.model.NoticeRemoteSource
 import com.doyoonkim.knutice.model.TopThreeNotices
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -9,43 +9,83 @@ import javax.inject.Inject
 
 
 class FetchTopThreeNoticeByCategory @Inject constructor (
-    private val remoteRepository: NoticeRemoteRepository
-) {
+    private val repository: NoticeLocalRepository
+): FetchTopThreeNotice {
 
-    private suspend fun fetchTopThreeNotices(category: NoticeCategory): Flow<TopThreeInCategory> {
-        Log.d("FetchTopThreeNoticeByCategory", "Start fetching")
-        return remoteRepository.getTopThreeNotice().map { response ->
-            if (response?.body != null) {
-                val result = response.body
-                val noticeList =  when (category) {
-                    NoticeCategory.GENERAL -> {
-                        result!!.latestThreeGeneralNews.toNotice()
-                    }
-                    NoticeCategory.ACADEMIC -> {
-                        result!!.latestThreeAcademicNews.toNotice()
-                    }
-                    NoticeCategory.SCHOLARSHIP -> {
-                        result!!.latestThreeScholarshipNews.toNotice()
-                    }
-                    NoticeCategory.EVENT -> {
-                        result!!.latestThreeEventNews.toNotice()
-                    }
-                }
-
+    override fun fetchTopThreeGeneralNotice(): Flow<TopThreeInCategory> {
+        return repository.getTopThreeNotice().map {
+            if (it.body != null) {
+                val latestGeneralNotices = it.body!!.latestThreeGeneralNews.toNotice()
                 TopThreeInCategory(
                     isSuccessful = true,
-                    notice1 = noticeList[0],
-                    notice2 = noticeList[1],
-                    notice3 = noticeList[2]
+                    notice1 = latestGeneralNotices[0],
+                    notice2 = latestGeneralNotices[1],
+                    notice3 = latestGeneralNotices[2]
                 )
-
             } else {
                 TopThreeInCategory(
                     isSuccessful = false
                 )
             }
-
         }
+    }
+
+    override fun fetchTopThreeAcademicNotice(): Flow<TopThreeInCategory> {
+        return repository.getTopThreeNotice().map {
+            if (it.body != null) {
+                val latestAcademicNotices = it.body!!.latestThreeAcademicNews.toNotice()
+                TopThreeInCategory(
+                    isSuccessful = true,
+                    notice1 = latestAcademicNotices[0],
+                    notice2 = latestAcademicNotices[1],
+                    notice3 = latestAcademicNotices[2]
+                )
+            } else {
+                TopThreeInCategory(
+                    isSuccessful = false
+                )
+            }
+        }
+    }
+
+    override fun fetchTopThreeScholarshipNotice(): Flow<TopThreeInCategory> {
+        return repository.getTopThreeNotice().map {
+            if (it.body != null) {
+                val latestScholarshipNotices = it.body!!.latestThreeScholarshipNews.toNotice()
+                TopThreeInCategory(
+                    isSuccessful = true,
+                    notice1 = latestScholarshipNotices[0],
+                    notice2 = latestScholarshipNotices[1],
+                    notice3 = latestScholarshipNotices[2]
+                )
+            } else {
+                TopThreeInCategory(
+                    isSuccessful = false
+                )
+            }
+        }
+    }
+
+    override fun fetchTopThreeEventNotice(): Flow<TopThreeInCategory> {
+        return repository.getTopThreeNotice().map {
+            if (it.body != null) {
+                val latestEventNotices = it.body!!.latestThreeEventNews.toNotice()
+                TopThreeInCategory(
+                    isSuccessful = true,
+                    notice1 = latestEventNotices[0],
+                    notice2 = latestEventNotices[1],
+                    notice3 = latestEventNotices[2]
+                )
+            } else {
+                TopThreeInCategory(
+                    isSuccessful = false
+                )
+            }
+        }
+    }
+
+    override fun fetchAllTopThreeNotice(): Flow<List<TopThreeInCategory>> {
+        TODO("Not yet implemented")
     }
 
     private fun ArrayList<TopThreeNotices.Body.LatestThreeNews>.toNotice(): List<Notice> {
@@ -58,8 +98,6 @@ class FetchTopThreeNoticeByCategory @Inject constructor (
             )
         }
     }
-
-    suspend operator fun invoke(category: NoticeCategory) = fetchTopThreeNotices(category)
 
 }
 
