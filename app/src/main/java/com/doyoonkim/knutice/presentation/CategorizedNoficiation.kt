2fab5c391b0a1eea4f59fdaa9aa561c9c34d4b2f
Doyoon Kim <doyoon3312@kakao.com>
2024-09-30 22:27:59 +0900

    [FEAT] Apply Compose Navigator
      - Apply Compose Navigator for moving between composables.
      - Separating reusable composable into independent file.
      - Remove unnecessary composable component

--- app/src/main/java/com/doyoonkim/knutice/presentation/CategorizedNoficiation.kt
+++ app/src/main/java/com/doyoonkim/knutice/presentation/CategorizedNoficiation.kt
@@ -1,27 +1,15 @@
 package com.doyoonkim.knutice.presentation
 
 import android.content.res.Configuration
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,30 +17,30 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.doyoonkim.knutice.domain.Notice
-import com.doyoonkim.knutice.ui.theme.containerBackground
+import androidx.navigation.NavController
+import com.doyoonkim.knutice.model.Destination
+import com.doyoonkim.knutice.model.Notice
+import com.doyoonkim.knutice.presentation.component.NotificationPreviewCard
 import com.doyoonkim.knutice.ui.theme.notificationType1
 import com.doyoonkim.knutice.ui.theme.notificationType2
 import com.doyoonkim.knutice.ui.theme.notificationType3
 import com.doyoonkim.knutice.ui.theme.notificationType4
 import com.doyoonkim.knutice.ui.theme.subTitle
-import com.doyoonkim.knutice.ui.theme.title
 import com.doyoonkim.knutice.viewModel.CategorizedNotificationViewModel
 import com.example.knutice.R
 
 @Composable
 fun CategorizedNotification(
     modifier: Modifier = Modifier,
-    viewModel: CategorizedNotificationViewModel = hiltViewModel()
+    viewModel: CategorizedNotificationViewModel = hiltViewModel(),
+    navController: NavController
 ) {
     val uiState by viewModel.uiState.collectAsState()
 
@@ -68,7 +56,7 @@ fun CategorizedNotification(
             titleColor = MaterialTheme.colorScheme.notificationType1,
             contents = uiState.notificationGeneral
         ) {
-            // TODO: onMoreClicked
+            navController.navigate(Destination.MORE_GENERAL.name)
         }
 
         NotificationPreviewList(
@@ -76,7 +64,7 @@ fun CategorizedNotification(
             titleColor = MaterialTheme.colorScheme.notificationType2,
             contents = uiState.notificationAcademic
         ) {
-            // TODO: onMoreClicked
+            navController.navigate(Destination.MORE_ACADEMIC.name)
         }
 
         NotificationPreviewList(
@@ -84,7 +72,7 @@ fun CategorizedNotification(
             titleColor = MaterialTheme.colorScheme.notificationType3,
             contents = uiState.notificationScholarship
         ) {
-            // TODO: onMoreClicked
+            navController.navigate(Destination.MORE_SCHOLARSHIP.name)
         }
 
         NotificationPreviewList(
@@ -92,9 +80,8 @@ fun CategorizedNotification(
             titleColor = MaterialTheme.colorScheme.notificationType4,
             contents = uiState.notificationEvent
         ) {
-            // TODO: onMoreClicked
+            navController.navigate(Destination.MORE_EVENT)
         }
-
     }
 }
 
@@ -136,7 +123,7 @@ fun NotificationPreviewList(
             )
         }
         contents.forEach { content ->
-            NotificationPreviewContainer(
+            NotificationPreviewCard(
                 notificationTitle = content.title,
                 notificationInfo = "[${content.departName}] ${content.timestamp}"
             ) {  }
@@ -145,95 +132,7 @@ fun NotificationPreviewList(
 }
 
 @Composable
-fun NotificationPreviewContainer(
-    notificationTitle: String = "Title goes here.",
-    notificationInfo: String = "Notification info goes here.",
-    onClick: () -> Unit = { /* Action should be defined. */ }
-) {
-    Card(
-        Modifier.fillMaxWidth()
-            .wrapContentHeight()
-            .clickable {
-                onClick()
-            },
-        colors = CardColors(
-            containerColor = MaterialTheme.colorScheme.containerBackground,
-            contentColor = Color.Unspecified,
-            disabledContainerColor = Color.Unspecified,
-            disabledContentColor = Color.Unspecified
-        ),
-        shape = RoundedCornerShape(10.dp)
-    ) {
-        Column(
-            Modifier.padding(10.dp),
-            verticalArrangement = Arrangement.spacedBy(7.dp)
-        ) {
-            if (notificationTitle == "Unknown") {
-                AnimatedGradient(Modifier.height(24.dp))
-                AnimatedGradient(Modifier.height(14.dp))
-            } else {
-                Text(
-                    modifier = Modifier.fillMaxWidth()
-                        .padding(top = 7.dp, start = 5.dp, end = 5.dp),
-                    text = notificationTitle,
-                    textAlign = TextAlign.Start,
-                    fontSize = 13.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    color = MaterialTheme.colorScheme.title
-                )
-                Text(
-                    modifier = Modifier.fillMaxWidth()
-                        .padding(top = 1.dp, start = 5.dp, bottom = 5.dp, end = 5.dp),
-                    text = notificationInfo,
-                    textAlign = TextAlign.Start,
-                    fontSize = 9.sp,
-                    color = MaterialTheme.colorScheme.subTitle
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun AnimatedGradient(
-    modifier: Modifier
-) {
-
-    val backgroundGray = Color(0xFF323232)
-    val backgroundMediumGray = Color(0xFF4C4C4C)
-    val backgroundLightGray = Color(0xFF5E5E5E)
-
-    val transition = rememberInfiniteTransition(label="placeholder")
-    val backgroundStart by transition.animateColor(
-        initialValue = backgroundGray,
-        targetValue = backgroundMediumGray,
-        animationSpec = infiniteRepeatable(
-            animation = tween(500, easing = LinearEasing),
-            repeatMode = RepeatMode.Reverse
-        ), label = "backgroundStart"
-    )
-
-    val backgroundEnd by transition.animateColor(
-        initialValue = backgroundMediumGray,
-        targetValue = backgroundLightGray,
-        animationSpec = infiniteRepeatable(
-            animation = tween(2000, easing = LinearEasing),
-            repeatMode = RepeatMode.Reverse
-        ), label = "backgroundEnd"
-    )
-
-    Box(
-        modifier = modifier.fillMaxWidth()
-            .background(
-                brush = Brush.linearGradient(listOf(backgroundStart, backgroundEnd)),
-                shape = RoundedCornerShape(3.dp)
-            )
-    )
-}
-
-
-@Composable
 @Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 fun CategorizedNotification_Preview() {
-    AnimatedGradient(Modifier.height(20.dp))
+
 }
