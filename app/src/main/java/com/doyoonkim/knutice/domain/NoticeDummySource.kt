170ac30ccdaff6bc4502e83f5388d72ee0f602f7
Doyoon Kim <doyoon3312@kakao.com>
2024-09-30 22:46:08 +0900

    [ETC] Class for creating dummy data.
      - Class for creating dummy data while simulating behavior of network communication.

--- app/src/main/java/com/doyoonkim/knutice/domain/NoticeDummySource.kt
+++ app/src/main/java/com/doyoonkim/knutice/domain/NoticeDummySource.kt
@@ -1,6 +1,8 @@
 package com.doyoonkim.knutice.domain
 
 import android.util.Log
+import com.doyoonkim.knutice.model.RawNoticeData
+import com.doyoonkim.knutice.model.Result
 import com.doyoonkim.knutice.model.TopThreeNotices
 import kotlinx.coroutines.delay
 
@@ -13,53 +15,53 @@ class NoticeDummySource {
             Log.d("NoticeRemoteSource", "Dummy Data Created")
             delay(1000)
             return TopThreeNotices(
-                result = TopThreeNotices.Result(
+                result = Result(
                     resultCode = 200,
                     resultMessage = "Dummy Data",
                     resultDescription = "DummyData Created"
                 ),
                 body = TopThreeNotices.Body(
                     latestThreeGeneralNews = arrayListOf(
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             1, "General 0", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         ),
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             2, "General 1", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         ),
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             3, "General 2", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         )
                     ),
                     latestThreeAcademicNews = arrayListOf(
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             1, "Academic 0", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         ),
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             2, "Academic 1", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         ),
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             3, "Academic 2", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         )
                     ),
                     latestThreeScholarshipNews = arrayListOf(
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             1, "Scholarship 0", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         ),
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             2, "Scholarship 1", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         ),
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             3, "Scholarship 2", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         )
                     ),
                     latestThreeEventNews = arrayListOf(
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             1, "Event 0", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         ),
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             2, "Event 1", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         ),
-                        TopThreeNotices.Body.LatestThreeNews(
+                        RawNoticeData(
                             3, "Event 2", "https:/www.ut.ac.kr/", "Dept", "2024-00-00"
                         )
                     )
