e4256c5d7a8eb0c37ac5f146d8cb66afc73e76be
Doyoon Kim <doyoon3312@kakao.com>
2024-09-30 22:53:19 +0900

    [REFACTOR] Separate Custom Types into a Single File.
      - Separate Custom Types (enum classes) into a single file.

---
+++ app/src/main/java/com/doyoonkim/knutice/model/Types.kt
@@ -0,0 +1,5 @@
+package com.doyoonkim.knutice.model
+
+enum class NoticeCategory { GENERAL_NEWS, ACADEMIC_NEWS, SCHOLARSHIP_NEWS, EVENT_NEWS, Unspecified }
+
+enum class Destination { MAIN, MORE_GENERAL, MORE_ACADEMIC, MORE_SCHOLARSHIP, MORE_EVENT, SETTINGS, CS }
