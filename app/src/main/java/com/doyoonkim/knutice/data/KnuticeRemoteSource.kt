92bb737e880eea98c0d4bdf0415d40450a9dea2b
Doyoon Kim <doyoon3312@kakao.com>
2024-10-14 23:06:22 +0900

    [FEAT] Firebase Cloud Message Service
      - Add new function in the RemoteSource that receive unique device token, and send it the the server for incoming push notification.

--- app/src/main/java/com/doyoonkim/knutice/data/KnuticeRemoteSource.kt
+++ app/src/main/java/com/doyoonkim/knutice/data/KnuticeRemoteSource.kt
@@ -3,36 +3,53 @@ package com.doyoonkim.knutice.data
 import android.util.Log
 import com.doyoonkim.knutice.model.NoticeCategory
 import com.doyoonkim.knutice.model.NoticesPerPage
+import com.doyoonkim.knutice.model.TokenInfo
 import com.doyoonkim.knutice.model.TopThreeNotices
+import com.doyoonkim.knutice.model.ValidateTokenResult
 import com.example.knutice.BuildConfig
+import com.google.gson.JsonObject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
 import retrofit2.http.Query
 import javax.inject.Inject
 
-class NoticeRemoteSource @Inject constructor() {
-    val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BuildConfig.API_ROOT)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+
+class KnuticeRemoteSource @Inject constructor() {
+
+    private object RetrofitInstance {
+        private val builder = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+
+        val noticeServiceInstance: Retrofit
+            get() = builder.baseUrl(BuildConfig.API_ROOT).build()
+
+        val fcmServiceInstance: Retrofit
+            get() = builder.baseUrl(BuildConfig.API_ROOT_FCM).build()
+    }
 
     suspend fun getTopThreeNotice(): TopThreeNotices {
-        Log.d("NoticeRemoteSource", "Start retrofit service")
-        return retrofit.create(KnuticeService::class.java).run {
+        Log.d("KnuticeRemoteSource", "Start retrofit service")
+        return RetrofitInstance.noticeServiceInstance.create(KnuticeService::class.java).run {
             this.getTopThreeNotice()
         }
     }
 
 
     suspend fun getNoticeListPerPage(category: NoticeCategory, lastNttId: Int): NoticesPerPage {
-        Log.d("NoticeRemoteSource", "Start retrofit service")
-        return retrofit.create(KnuticeService::class.java).run {
+        Log.d("KnuticeRemoteSource", "Start retrofit service")
+        return RetrofitInstance.noticeServiceInstance.create(KnuticeService::class.java).run {
             if (lastNttId == 0) {
                 this.getFirstPageOfNotice(category)
             } else {
@@ -48,6 +65,14 @@ class NoticeRemoteSource @Inject constructor() {
                 .getElementsByClass("bbs-view-content bbs-view-content-skin05")
                 .text() ?: "Unable to receive full notice content"
         }
+
+    fun validateToken(token: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            RetrofitInstance.fcmServiceInstance.create(KnuticeService::class.java).validateToken(
+                TokenInfo(deviceToken = token)
+            )
+        }
+    }
 
 }
 
@@ -66,5 +91,11 @@ interface KnuticeService {
     suspend fun getFirstPageOfNotice(
         @Query("noticeName") category: NoticeCategory
     ): NoticesPerPage
+
+    @Headers("Content-Type: application/json")
+    @POST("/fcm/sendDeviceToken")
+    suspend fun validateToken(
+        @Body requestBody: TokenInfo
+    ): ValidateTokenResult
 
 }
