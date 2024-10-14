2089616eb20f5961d4cc1c3cfd4a4419f6147445
Doyoon Kim <doyoon3312@kakao.com>
2024-10-14 23:08:01 +0900

    [FEAT] Firebase Cloud Message Service
      - Triggers function that fetches current device token generated by FCM service, and pass it to the function in the remote source in order to send it to the server.

--- app/src/main/java/com/doyoonkim/knutice/MainApplication.kt
+++ app/src/main/java/com/doyoonkim/knutice/MainApplication.kt
@@ -1,13 +1,17 @@
 package com.doyoonkim.knutice
 
 import android.app.Application
+import com.doyoonkim.knutice.fcm.PushNotificationHandler
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication : Application() {
+class MainApplication() : Application() {
+    @Inject lateinit var notificationHandler: PushNotificationHandler
 
     override fun onCreate() {
         super.onCreate()
+        notificationHandler.requestCurrentToken()
     }
 
     override fun onTerminate() {
