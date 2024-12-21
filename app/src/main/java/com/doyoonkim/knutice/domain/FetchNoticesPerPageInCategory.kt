415b80d50f8492240ba609529a754ab5b74f0c26
Doyoon Kim <doyoon3312@kakao.com>
2024-12-21 23:43:42 +0900

    [REFACT] Change Interface structure.
      - Change structure of interface 'FetchListOfNotices' for reusability.

--- app/src/main/java/com/doyoonkim/knutice/domain/FetchNoticesPerPageInCategory.kt
+++ app/src/main/java/com/doyoonkim/knutice/domain/FetchNoticesPerPageInCategory.kt
@@ -3,7 +3,6 @@ package com.doyoonkim.knutice.domain
 import com.doyoonkim.knutice.data.NoticeLocalRepository
 import com.doyoonkim.knutice.model.Notice
 import com.doyoonkim.knutice.model.NoticeCategory
-import com.doyoonkim.knutice.model.RawNoticeData
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -18,17 +17,8 @@ class FetchNoticesPerPageInCategory @Inject constructor(
         }
     }
 
-    private fun ArrayList<RawNoticeData>.toNotice(): List<Notice> {
-        return List<Notice>(this.size) { index ->
-            Notice(
-                nttId = this[index].nttId ?: -1,
-                title = this[index].title ?: "Unknown",
-                url = this[index].contentUrl ?: "Unknown",
-                imageUrl = this[index].contentImage ?: "Unknown",
-                departName = this[index].departName ?: "Unknown",
-                timestamp = this[index].registeredAt ?: "Unknown"
-            )
-        }
+    override fun getNoticesByKeyword(keyword: String): Flow<List<Notice>> {
+        TODO("Does not required to be implemented.")
     }
 
 }
