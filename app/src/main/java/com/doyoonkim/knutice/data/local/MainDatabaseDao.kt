878927108ffc3926b5f7106dd3a282ad6b09b0f2
Doyoon Kim <doyoon3312@kakao.com>
2025-02-23 23:19:16 +0900

    [FEAT] Bookmark Feature   Overall Description: Users are now being able to add/modify/remove bookmark regards to certain notice.
      - Include new CRUD functions to DAO.

--- app/src/main/java/com/doyoonkim/knutice/data/local/MainDatabaseDao.kt
+++ app/src/main/java/com/doyoonkim/knutice/data/local/MainDatabaseDao.kt
@@ -7,6 +7,7 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.doyoonkim.knutice.model.Bookmark
+import com.doyoonkim.knutice.model.NoticeEntity
 
 @Dao
 interface MainDatabaseDao {
@@ -16,9 +17,22 @@ interface MainDatabaseDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun createBookmark(entity: Bookmark)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun createNoticeEntity(entity: NoticeEntity)
+
     @Update
     fun updateBookmark(updated: Bookmark)
 
     @Delete
     fun deleteBookmark(target: Bookmark)
+
+    @Delete
+    fun deleteNoticeEntity(target: NoticeEntity)
+
+    @Query("SELECT * FROM NoticeEntity WHERE ntt_id=:nttId")
+    fun getNoticeByNttId(nttId: Int): NoticeEntity
+
+    @Query("SELECT * FROM Bookmark WHERE target_ntt_id=:nttId")
+    fun getBookmarkByNttId(nttId: Int): Bookmark?
+
 }
