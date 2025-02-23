b00e0ce7613242dcbf9c945c5e84ce3fddc967b9
Doyoon Kim <doyoon3312@kakao.com>
2025-02-23 23:38:22 +0900

    [FEAT] Bookmark Feature   Overall Description: Users are now being able to add/modify/remove bookmark regards to certain notice.
      - Add new function belongs to Notice and NoticeEntity data class. (toNoticeEntity, toNotice)

--- app/src/main/java/com/doyoonkim/knutice/model/DataWrappers.kt
+++ app/src/main/java/com/doyoonkim/knutice/model/DataWrappers.kt
@@ -2,6 +2,7 @@ package com.doyoonkim.knutice.model
 
 import androidx.room.Entity
 import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
 
 
 data class Result(
@@ -81,6 +82,7 @@ data class ManageTopicRequest(
 
 // Data class to be applied to uiState.
 // Universal
+@Serializable
 data class Notice(
     val nttId: Int = -1,
     val title: String = "Unknown",
@@ -88,7 +90,28 @@ data class Notice(
     val imageUrl: String = "",
     val departName: String = "Unknown",
     val timestamp: String = "Unknown"
-)
+) {
+    fun toFullContent(): FullContent {
+        return FullContent(
+            title,
+            "[$departName] $timestamp",
+            url,
+            imageUrl
+        )
+    }
+
+    fun toNoticeEntity(): NoticeEntity {
+        return NoticeEntity(
+            noticeEntityId = 0,
+            nttId = nttId,
+            title = title,
+            url = url,
+            imageUrl = imageUrl,
+            departName = departName,
+            timestamp = timestamp
+        )
+    }
+}
 
 // DetailedNoticeContent
 data class DetailedContentState(
@@ -125,8 +148,18 @@ data class NotificationPreferenceStatus(
 
 // BookmarkComposable
 data class BookmarkComposableState(
-    val bookmarks: List<Bookmark> = emptyList(),
+    val bookmarks: List<Pair<Bookmark, Notice>> = emptyList(),
     val isRefreshing: Boolean = false,
     val isRefreshRequested: Boolean = false
+)
+
+// EditBookmark
+data class EditBookmarkState(
+    val bookmarkId: Int = 0,
+    val targetNotice: Notice = Notice(),
+    val isReminderRequested: Boolean = false,
+    val timeForRemind: Long = 0,      // Should be replaced with an appropriate data type later.
+    val bookmarkNote: String = "",
+    val requireCreation: Boolean = true
 )
 
