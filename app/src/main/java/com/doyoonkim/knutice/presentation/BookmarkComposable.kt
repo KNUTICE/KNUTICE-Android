3ad26faed113fc4244a66d3443d3d7e5ac7a4fd7
Doyoon Kim <doyoon3312@kakao.com>
2025-02-24 23:27:11 +0900

    [FEAT] Bookmark Feature   Overall Description: Users are now being able to add/modify/remove bookmark regards to certain notice.
      - Fix duplicated bookmark to be shown on LazyColumn. (Remove identical elements from the list.)

--- app/src/main/java/com/doyoonkim/knutice/presentation/BookmarkComposable.kt
+++ app/src/main/java/com/doyoonkim/knutice/presentation/BookmarkComposable.kt
@@ -1,33 +1,49 @@
 package com.doyoonkim.knutice.presentation
 
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.doyoonkim.knutice.model.Notice
 import com.doyoonkim.knutice.presentation.component.NotificationPreviewCardMarked
 import com.doyoonkim.knutice.ui.theme.displayBackground
 import com.doyoonkim.knutice.viewModel.BookmarkViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun BookmarkComposable(
     modifier: Modifier = Modifier,
-    viewModel: BookmarkViewModel = hiltViewModel()
+    viewModel: BookmarkViewModel = hiltViewModel(),
+    onEachItemClicked: (Notice) -> Unit = {  }
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+
+//    LaunchedEffect(Unit) {
+//        viewModel.getAllBookmarks()
+//    }
+    SideEffect {
+        viewModel.getAllBookmarks()
+        Log.d("BookmarkComposable", "Side Effect Called")
+    }
+
     Box(
         modifier = modifier.background(MaterialTheme.colorScheme.displayBackground)
             .systemBarsPadding()
@@ -35,12 +51,19 @@ fun BookmarkComposable(
         LazyColumn(
             modifier = Modifier.wrapContentHeight()
                 .fillMaxWidth()
+                .padding(top = 12.dp)
                 .background(Color.Transparent),
-            verticalArrangement = Arrangement.SpaceBetween,
+            verticalArrangement = Arrangement.spacedBy(12.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            items(1) {
-                NotificationPreviewCardMarked()
+            items(uiState.bookmarks) {
+                // Being called 3 times
+                Log.d("BookmarkComposable", "Element: $it")
+                NotificationPreviewCardMarked(
+                    noticeTitle = it.second.title,
+                    noticeSubtitle = "[${it.second.departName}] ${it.second.timestamp}",
+                    onItemClicked = { onEachItemClicked(it.second) }
+                )
             }
         }
     }
