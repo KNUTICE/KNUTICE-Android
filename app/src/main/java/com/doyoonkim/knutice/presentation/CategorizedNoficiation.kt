35acfb96fa15a907b33e4ac1f785bcbb50d0990f
Doyoon Kim <doyoon3312@kakao.com>
2025-02-24 23:26:03 +0900

    [FEAT] Bookmark Feature   Overall Description: Users are now being able to add/modify/remove bookmark regards to certain notice.
      - Replace lambda function onNoticeClicked has a enhanced parameter type.

--- app/src/main/java/com/doyoonkim/knutice/presentation/CategorizedNoficiation.kt
+++ app/src/main/java/com/doyoonkim/knutice/presentation/CategorizedNoficiation.kt
@@ -1,19 +1,21 @@
 package com.doyoonkim.knutice.presentation
 
 import android.content.res.Configuration
-import android.util.Log
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -44,7 +46,7 @@ fun CategorizedNotification(
     viewModel: CategorizedNotificationViewModel = hiltViewModel(),
     onGoBackAction: () -> Unit,
     onMoreNoticeRequested: (Destination) -> Unit,
-    onFullContentRequested: (FullContent) -> Unit
+    onFullContentRequested: (Notice) -> Unit
 ) {
     val uiState by viewModel.uiState.collectAsState()
 
@@ -65,8 +67,8 @@ fun CategorizedNotification(
             titleColor = MaterialTheme.colorScheme.notificationType1,
             contents = uiState.notificationGeneral,
             onMoreClicked = { onMoreNoticeRequested(Destination.MORE_GENERAL) }
-        ) { title, info, url, imgUrl ->
-            onFullContentRequested(FullContent(title, info, url, imgUrl))
+        ) {
+            onFullContentRequested(it)
         }
 
         NotificationPreviewList(
@@ -74,8 +76,8 @@ fun CategorizedNotification(
             titleColor = MaterialTheme.colorScheme.notificationType2,
             contents = uiState.notificationAcademic,
             onMoreClicked = { onMoreNoticeRequested(Destination.MORE_ACADEMIC) }
-        ) { title, info, url, imgUrl ->
-            onFullContentRequested(FullContent(title, info, url, imgUrl))
+        ) {
+            onFullContentRequested(it)
         }
 
         NotificationPreviewList(
@@ -83,8 +85,8 @@ fun CategorizedNotification(
             titleColor = MaterialTheme.colorScheme.notificationType3,
             contents = uiState.notificationScholarship,
             onMoreClicked = { onMoreNoticeRequested(Destination.MORE_SCHOLARSHIP) }
-        ) { title, info, url, imgUrl ->
-            onFullContentRequested(FullContent(title, info, url, imgUrl))
+        ) {
+            onFullContentRequested(it)
         }
 
         NotificationPreviewList(
@@ -92,8 +94,8 @@ fun CategorizedNotification(
             titleColor = MaterialTheme.colorScheme.notificationType4,
             contents = uiState.notificationEvent,
             onMoreClicked = { onMoreNoticeRequested(Destination.MORE_EVENT) }
-        ) { title, info, url, imgUrl ->
-            onFullContentRequested(FullContent(title, info, url, imgUrl))
+        ) {
+            onFullContentRequested(it)
         }
     }
 }
@@ -105,7 +107,7 @@ fun NotificationPreviewList(
     titleColor: Color = Color.Unspecified,
     contents: List<Notice> = listOf(),
     onMoreClicked: () -> Unit = {  },
-    onNoticeClicked: (String, String, String, String) -> Unit
+    onNoticeClicked: (Notice) -> Unit
 ) {
     Column(
         modifier = Modifier.fillMaxWidth()
@@ -114,9 +116,11 @@ fun NotificationPreviewList(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(
-            Modifier.fillMaxWidth()
+            Modifier.fillMaxWidth(
+
+            )
                 .wrapContentHeight(),
-            verticalAlignment = Alignment.Bottom,
+            verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
             Text(
@@ -127,33 +131,36 @@ fun NotificationPreviewList(
                 fontWeight = FontWeight.Bold
             )
 
-            Text(
-                modifier = Modifier.fillMaxWidth().weight(1f)
-                    .clickable { onMoreClicked() },
-                text = stringResource(R.string.btn_more),
-                color = MaterialTheme.colorScheme.subTitle,
-                fontSize = 13.sp,
-                fontWeight = FontWeight.Medium
-            )
+            TextButton(
+                modifier = Modifier.fillMaxWidth().weight(1f),
+                onClick = { onMoreClicked() },
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Text(
+                    modifier = Modifier.align(Alignment.CenterVertically),
+                    text = stringResource(R.string.btn_more),
+                    color = MaterialTheme.colorScheme.subTitle,
+                    fontWeight = FontWeight.Medium
+                )
+            }
         }
         contents.forEach { content ->
             NotificationPreviewCard(
                 notificationTitle = content.title,
                 notificationInfo = "[${content.departName}] ${content.timestamp}"
             ) {
-                onNoticeClicked(
-                    content.title,
-                    "[${content.departName}] ${content.timestamp}",
-                    content.url,
-                    content.imageUrl
-                )
+                onNoticeClicked(content)
             }
         }
     }
 }
 
 @Composable
-@Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, showSystemUi = false, uiMode = Configuration.UI_MODE_NIGHT_YES)
 fun CategorizedNotification_Preview() {
+    NotificationPreviewList(
+        contents = listOf(Notice(), Notice(), Notice())
+    ) {
 
+    }
 }
