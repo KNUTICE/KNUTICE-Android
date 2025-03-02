3648594806f6799b3fa62fd25d48a79fecff0bed
Doyoon Kim <doyoon3312@kakao.com>
2025-03-02 17:50:14 +0900

    [REFACT] Add Blank page for List of Bookmarks
      - Add blank page for list of Bookmarks that informs user that there are no bookmarks saved.

--- app/src/main/java/com/doyoonkim/knutice/presentation/BookmarkComposable.kt
+++ app/src/main/java/com/doyoonkim/knutice/presentation/BookmarkComposable.kt
@@ -13,21 +13,24 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.doyoonkim.knutice.R
 import com.doyoonkim.knutice.model.Notice
 import com.doyoonkim.knutice.presentation.component.NotificationPreviewCardMarked
 import com.doyoonkim.knutice.ui.theme.displayBackground
+import com.doyoonkim.knutice.ui.theme.subTitle
 import com.doyoonkim.knutice.viewModel.BookmarkViewModel
-import kotlinx.coroutines.launch
 
 @Composable
 fun BookmarkComposable(
@@ -50,25 +53,35 @@ fun BookmarkComposable(
         modifier = modifier.background(MaterialTheme.colorScheme.displayBackground)
             .systemBarsPadding()
     ) {
-        LazyColumn(
-            modifier = Modifier.wrapContentHeight()
-                .fillMaxWidth()
-                .padding(top = 12.dp)
-                .background(Color.Transparent),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            items(
-                items = uiState.bookmarks,
-                key = { it.second.nttId }
+        if (uiState.bookmarks.isEmpty()) {
+            Text(
+                text = stringResource(R.string.text_no_bookmark),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = MaterialTheme.colorScheme.subTitle,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        } else {
+            LazyColumn(
+                modifier = Modifier.wrapContentHeight()
+                    .fillMaxWidth()
+                    .padding(top = 12.dp)
+                    .background(Color.Transparent),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                // Being called 3 times
-                Log.d("BookmarkComposable", "Element: $it")
-                NotificationPreviewCardMarked(
-                    noticeTitle = it.second.title,
-                    noticeSubtitle = "[${it.second.departName}] ${it.second.timestamp}",
-                    onItemClicked = { onEachItemClicked(it.second) }
-                )
+                items(
+                    items = uiState.bookmarks,
+                    key = { it.second.nttId }
+                ) {
+                    // Being called 3 times
+                    Log.d("BookmarkComposable", "Element: $it")
+                    NotificationPreviewCardMarked(
+                        noticeTitle = it.second.title,
+                        noticeSubtitle = "[${it.second.departName}] ${it.second.timestamp}",
+                        onItemClicked = { onEachItemClicked(it.second) }
+                    )
+                }
             }
         }
     }
