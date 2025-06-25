670d0becd4f69a10b449469d70709c32d761412e
Doyoon Kim <doyoon3312@kakao.com>
2025-06-25 17:59:09 +0900

    [CHORE] Version Code Changes
      - 20 -> 21 (1.4.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "com.doyoonkim.knutice"
         minSdk = 31
         targetSdk = 34
-        versionCode = 20
+        versionCode = 21
         versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
