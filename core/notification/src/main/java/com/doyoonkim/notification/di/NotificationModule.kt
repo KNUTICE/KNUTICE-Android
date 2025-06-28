85f5147286df936da533781f73991a28ce2b0fd9
Doyoon Kim <doyoon3312@kakao.com>
2025-06-28 17:01:06 +0900

    [REFACTOR] Add Additional Module for Binding Instances
      - Add Additional @Module in :notification/di for binding instances.

--- core/notification/src/main/java/com/doyoonkim/notification/di/NotificationModule.kt
+++ core/notification/src/main/java/com/doyoonkim/notification/di/NotificationModule.kt
@@ -3,9 +3,12 @@ package com.doyoonkim.notification.di
 import android.content.Context
 import com.doyoonkim.common.BitmapHandler
 import com.doyoonkim.common.di.ApplicationContext
+import com.doyoonkim.common.di.TokenHandler
 import com.doyoonkim.domain.ImageRepository
 import com.doyoonkim.domain.usecases.ValidateDeviceToken
 import com.doyoonkim.notification.fcm.PushNotificationHandler
+import com.doyoonkim.notification.fcm.TokenHandlerImpl
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -26,4 +29,12 @@ object NotificationModule {
             context
         )
 
+}
+
+@Module
+abstract class NotificationBindingModule {
+    @Binds
+    abstract fun bindsTokenHandler(
+        impl: TokenHandlerImpl
+    ): TokenHandler
 }
