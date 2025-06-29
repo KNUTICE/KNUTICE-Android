6259103a7d3ad46d18d37cb51bd9635733642b5f
Doyoon Kim <doyoon3312@kakao.com>
2025-06-29 22:30:36 +0900

    [REFACTOR] Enhance the Method to Retrieve Device Token and Validate it
      - Replace Listener-based approach to retrieve FCM token to Coroutine-based await() approach to retrieve FCM token.

--- core/notification/src/main/java/com/doyoonkim/notification/fcm/TokenHandler.kt
+++ core/notification/src/main/java/com/doyoonkim/notification/fcm/TokenHandler.kt
@@ -4,16 +4,11 @@ import android.util.Log
 import com.doyoonkim.common.di.TokenHandler
 import com.doyoonkim.domain.usecases.ValidateDeviceToken
 import com.doyoonkim.model.requestBody.DeviceTokenBody
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collectLatest
+import com.google.firebase.Firebase
+import com.google.firebase.messaging.messaging
+import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class TokenHandlerImpl @Inject constructor(
@@ -22,33 +17,8 @@ class TokenHandlerImpl @Inject constructor(
     private val TAG = this.javaClass.name
 
     override fun handleCurrentTokenRequest() = flow {
-        runCatching {
-            FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.d(TAG, "Incomplete task: ${task.exception}")
-                    throw Exception(task.exception?.message)
-                }
-
-                // Get new FCM registration token
-                val registrationToken = task.result
-                Log.d(TAG, "Received Token: $registrationToken")
-
-                // POST request to upload current token to the web server.
-                CoroutineScope(Dispatchers.IO).launch {
-                    Log.d(TAG, "Start validating Token")
-                    validateDeviceToken(DeviceTokenBody(fcmToken = registrationToken))
-                        .collectLatest { result ->
-                            if (result) Log.d(TAG, "Validation Successful")
-                            else throw Exception("Unable to validate")
-                        }
-                }
-            })
-        }.fold(
-            onSuccess = { emit(true) },
-            onFailure = {
-                println("Unable to validate: ${it.message}")
-                emit(false)
-            }
-        )
+        val deviceToken = Firebase.messaging.token.await()
+        Log.d(TAG, "Received Token: $deviceToken")
+        emitAll(validateDeviceToken(DeviceTokenBody(fcmToken = deviceToken)))
     }
 }
