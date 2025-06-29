be33d0c74ba2c9b17ddff118fa57c12e88a8203f
Doyoon Kim <doyoon3312@kakao.com>
2025-06-29 23:10:57 +0900

    [REFACTOR] Remove Unnecessary onFailure Block
      - Remove unnecessary onFailure block that potentially causing unhandled exception.
      - Now all exceptions occurred during request via remoteSource are being handled at Repository.

--- core/network/src/main/kotlin/com/doyoonkim/network/KnuticeRemoteSource.kt
+++ core/network/src/main/kotlin/com/doyoonkim/network/KnuticeRemoteSource.kt
@@ -31,31 +31,31 @@ class KnuticeRemoteSource @Inject constructor() {
 
     suspend fun getTopThreeNotices() = runCatching {
             knuticeService.create(KnuticeService::class.java).getTopThreeNotices()
-        }.onFailure { throw it }
+        }
 
     suspend fun getNoticesPerPage(category: NoticeCategory, lastNttId: Int?) = runCatching {
             knuticeService.create(KnuticeService::class.java).getNoticesPerPage(category, lastNttId)
-        }.onFailure { throw it }
+        }
 
     suspend fun getNoticeById(nttId: Int) = runCatching {
             knuticeService.create(KnuticeService::class.java).getNoticeById(nttId.toString())
-        }.onFailure { throw it }
+        }
 
 
     suspend fun getNoticesByKeyword(keyword: String) = runCatching {
             knuticeService.create(KnuticeService::class.java).getNoticesByKeyword(keyword)
-        }.onFailure { throw it }
+        }
 
     suspend fun getTopicSubscriptionStatus() = runCatching {
             if (validatedToken.isBlank()) throw Exception("No validated token found")
             else {
                 knuticeService.create(KnuticeService::class.java).getTopicSubscriptionStatus(validatedToken)
             }
-        }.onFailure { throw it }
+        }
 
     suspend fun validateToken(request: DeviceTokenRequest) = runCatching {
             knuticeService.create(KnuticeService::class.java).validateToken(request)
-        }.onFailure { throw it }
+        }
 
     fun updateValidatedToken(fcmToken: String) {
         validatedToken = fcmToken
@@ -63,10 +63,10 @@ class KnuticeRemoteSource @Inject constructor() {
 
     suspend fun submitUserReport(request: UserReportRequest) = runCatching {
             knuticeService.create(KnuticeService::class.java).submitUserReport(request)
-        }.onFailure { throw it }
+        }
 
     suspend fun submitTopicSubscriptionPreferences(request: TopicSubscriptionPreferencesRequest) =
         runCatching {
             knuticeService.create(KnuticeService::class.java).submitTopicSubscriptionPreferences(request)
-        }.onFailure { throw it }
+        }
 }
