7a97cfec215e5241d956f08d5b7a509ef92f5e0a
Doyoon Kim <doyoon3312@kakao.com>
2025-07-02 00:50:15 +0900

    [CHORE] Version Information Changes
      - Version Code: 23
      - Version Name: 1.4.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.doyoonkim.knutice"
         minSdk = 31
         targetSdk = 34
-        versionCode = 22
-        versionName = "1.4.2.1"
+        versionCode = 23
+        versionName = "1.4.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
