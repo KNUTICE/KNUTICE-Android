d160edc42f2810616a4cc574d4bae5b6621b4ead
Doyoon Kim <doyoon3312@kakao.com>
2025-07-19 22:16:18 +0900

    [FEAT] Set Employment Notices to be shown
      - Modify DTO and VO to accept list of Employment Notices

--- core/model/src/main/java/com/doyoonkim/model/TopThreeNoticeVO.kt
+++ core/model/src/main/java/com/doyoonkim/model/TopThreeNoticeVO.kt
@@ -4,5 +4,6 @@ data class TopThreeNoticeVO(
     val general: List<NoticeVO> = listOf(),
     val scholarship: List<NoticeVO> = listOf(),
     val event: List<NoticeVO> = listOf(),
-    val academic: List<NoticeVO> = listOf()
+    val academic: List<NoticeVO> = listOf(),
+    val employment: List<NoticeVO> = listOf()
 )
