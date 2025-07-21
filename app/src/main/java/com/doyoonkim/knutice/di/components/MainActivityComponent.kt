b80c0283e27fa52cb94b9e2bf4442948f8b97b4e
Doyoon Kim <doyoon3312@kakao.com>
2025-07-21 23:43:24 +0900

    KAN-36 [REFACTOR] Improve DI Structure
      - Remove unnecessary module dependencies from MainActivityComponent.

--- app/src/main/java/com/doyoonkim/knutice/di/components/MainActivityComponent.kt
+++ app/src/main/java/com/doyoonkim/knutice/di/components/MainActivityComponent.kt
@@ -1,37 +1,18 @@
 package com.doyoonkim.knutice.di.components
 
-import android.app.Application
-import com.doyoonkim.bookmark.di.BookmarkModule
-import com.doyoonkim.common.di.CommonModule
-import com.doyoonkim.data.di.DataModule
-import com.doyoonkim.domain.di.DomainModule
+
 import com.doyoonkim.knutice.MainActivity
-import com.doyoonkim.knutice.di.modules.AppModule
-import com.doyoonkim.knutice.di.modules.DispatcherModule
-import com.doyoonkim.knutice.di.modules.ViewModelFactoryModule
-import com.doyoonkim.main.di.MainModule
-import com.doyoonkim.network.di.NetworkModule
-import com.doyoonkim.notification.di.NotificationModule
-import dagger.BindsInstance
+import com.doyoonkim.knutice.di.util.SystemServices
 import dagger.Component
 
-@Component(modules = [
-    AppModule::class,
-    DispatcherModule::class,
-    CommonModule::class,
-    DataModule::class,
-    DomainModule::class,
-    NetworkModule::class,
-    NotificationModule::class,
-    BookmarkModule::class,
-    MainModule::class,
-    ViewModelFactoryModule::class
-])
+@Component(
+    dependencies = [SystemServices::class]
+)
 interface MainActivityComponent {
     fun inject(activity: MainActivity)
 
     @Component.Factory
     interface Factory {
-        fun create(@BindsInstance application: Application): MainActivityComponent
+        fun create(systemServices: SystemServices): MainActivityComponent
     }
 }
