9ede594a4c6c53c35332d2279b5cc8745174925b
Doyoon Kim <doyoon3312@kakao.com>
2025-08-02 23:50:37 +0900

    [FEAT] Local Bookmark Search Feature   * Allow user to search locally stored bookmark by keyword
        - Add CRUD function execution for FTS-enabled table alongside with CRUD function execution for Original Bookmark/NoticeEntity tables.

--- core/domain/src/main/java/com/doyoonkim/domain/usecases/ModifyBookmark.kt
+++ core/domain/src/main/java/com/doyoonkim/domain/usecases/ModifyBookmark.kt
@@ -3,15 +3,22 @@ package com.doyoonkim.domain.usecases
 import com.doyoonkim.domain.interfaces.BookmarkLocalRepository
 import com.doyoonkim.domain.interfaces.NoticeLocalRepository
 import com.doyoonkim.domain.interfaces.NoticeRemoteRepository
+import com.doyoonkim.domain.util.KoreanTokenizer
+import com.doyoonkim.model.BookmarkFtsVO
 import com.doyoonkim.model.BookmarkVO
 import com.doyoonkim.model.NoticeVO
+import com.doyoonkim.model.di.DefaultDispatcher
 import com.doyoonkim.model.di.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 interface ModifyBookmark {
@@ -27,7 +34,8 @@ class ModifyBookmarkImpl @Inject constructor(
     private val noticeLocalRepository: NoticeLocalRepository,
     private val bookmarkLocalRepository: BookmarkLocalRepository,
     private val remoteRepository: NoticeRemoteRepository,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
 ) : ModifyBookmark {
 
     override fun query(nttId: Int): Flow<BookmarkVO> =
@@ -38,30 +46,73 @@ class ModifyBookmarkImpl @Inject constructor(
         }.flowOn(ioDispatcher)
 
     // TODO: Need to be revised later. (Is NoticeVO really required)
-    override fun createOrUpdate(bookmark: BookmarkVO, notice: NoticeVO?): Flow<Boolean> {
-        return if (notice == null) {
+    override fun createOrUpdate(bookmark: BookmarkVO, notice: NoticeVO?) = flow {
+        if (notice == null) {
             // Need creation. Request notice instance from the remote source first.
             remoteRepository.queryNoticeById(bookmark.targetNoticeNttId).transform { result ->
-                if (result != null) emitAll(bookmarkLocalRepository.createBookmark(bookmark, result))
+                if (result != null) {
+                    emitAll(
+                        combine(
+                            bookmarkLocalRepository.createBookmark(bookmark, result),
+                            bookmarkLocalRepository.createBookmarkFts(
+                                generateBookmarkFtsEntry(bookmark, result)
+                            )
+                        ) { first, second ->
+                            first && second
+                        }
+                    )
+                }
                 else emit(false)
             }.catch {
                 /* Internal Error. Consume values, and never emit values. */
             }.flowOn(ioDispatcher)
         } else {
-            bookmarkLocalRepository.updateBookmark(bookmark)
-                .catch {
-                    /* Internal Error. Consume values, and never emit values. */
-                }.flowOn(ioDispatcher)
+            emitAll(
+                combine(
+                    bookmarkLocalRepository.updateBookmark(bookmark),
+                    bookmarkLocalRepository.updateBookmarkFts(
+                        generateBookmarkFtsEntry(bookmark, notice)
+                    )
+                ) { first, second ->
+                    first && second
+                }
+            )
         }
     }
 
     override fun delete(bookmark: BookmarkVO, notice: NoticeVO): Flow<Boolean> =
         bookmarkLocalRepository.requestBookmarkDeletion(bookmark).transform { result ->
-            if (result) emitAll(noticeLocalRepository.requestNoticeDeletion(notice))
+            if (result) {
+                emitAll(
+                    combine(
+                        noticeLocalRepository.requestNoticeDeletion(notice),
+                        bookmarkLocalRepository.deleteBookmarkFts(
+                            generateBookmarkFtsEntry(bookmark, notice)
+                        )
+                    ) { first, second ->
+                        first && second
+                    }
+                )
+            }
             else emit(false)
         }.catch {
             /* Internal Error. Consume values, and never emit values. */
         }.flowOn(ioDispatcher)
+
+
+    private suspend fun generateBookmarkFtsEntry(bookmark: BookmarkVO, notice: NoticeVO): BookmarkFtsVO {
+        return BookmarkFtsVO(
+            ftsId = bookmark.bookmarkId,
+            bookmarkNote = bookmark.bookmarkNote,
+            noticeTitle = notice.title,
+            bookmarkNoteTokenized = withContext(defaultDispatcher) {
+                async { KoreanTokenizer.getTokenizedString(bookmark.bookmarkNote) }
+            }.await(),
+            noticeTitleTokenized = withContext(defaultDispatcher) {
+                async { KoreanTokenizer.getTokenizedString(notice.title) }
+            }.await()
+        )
+    }
 
 }
 
