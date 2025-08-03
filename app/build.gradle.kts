9992ab91fa8b9f382a2fa2d9c51be6d3d41a9392
Doyoon Kim <doyoon3312@kakao.com>
2025-08-03 22:36:25 +0900

    [CHORE] Update Version Information
      - Update Version Information
        * Version Code: 27
        * Version name: 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.doyoonkim.knutice"
         minSdk = 31
         targetSdk = 35
-        versionCode = 26
-        versionName = "1.5.0"
+        versionCode = 27
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
