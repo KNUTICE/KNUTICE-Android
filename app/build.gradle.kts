167c287bb70f95bc3a0ce2019f11a1da4b0f31bb
Doyoon Kim <doyoon3312@kakao.com>
2025-10-09 03:23:54 -0400

    [CHORE] Update Version Information
      - Version Code: 30
      - Version Name: 1.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.doyoonkim.knutice"
         minSdk = 31
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.5.2"
+        versionCode = 30
+        versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
