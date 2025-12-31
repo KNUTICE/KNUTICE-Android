0098a65448d3a86bf5b02c4fb0d6b20579db8d46
Doyoon Kim <doyoon3312@kakao.com>
2025-12-31 23:33:33 +0900

    KAN-76 [REFACTOR] Apply Pure Reducer for State Management
      - Improve MVI pattern applied. Use Pure Reducer for State Management. (Centralized State Modification Logic)
        * (HomeViewModel) Implement main reducer with an appropriate UiMutation.
        * (Contract) Declare corresponding UiMutation. Separate centralized UiState. (MainContentState and TipState)
        * (Screen) Apply corresponding modification cased by State separation.

--- feature/main/src/main/java/com/doyoonkim/main/viewmodel/HomeViewModel.kt
+++ feature/main/src/main/java/com/doyoonkim/main/viewmodel/HomeViewModel.kt
@@ -1,13 +1,17 @@
 package com.doyoonkim.main.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.doyoonkim.common.base.BaseViewModel
 import com.doyoonkim.common.navigation.Destination
 import com.doyoonkim.domain.usecases.FetchTips
 import com.doyoonkim.domain.usecases.FetchTopThreeNotices
 import com.doyoonkim.main.contract.HomeEvent
+import com.doyoonkim.main.contract.HomeMutation
 import com.doyoonkim.main.contract.HomeSideEffect
 import com.doyoonkim.main.contract.HomeViewState
+import com.doyoonkim.main.contract.MainContentState
+import com.doyoonkim.main.contract.TipState
 import com.doyoonkim.model.NoticeCategory
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -16,7 +20,7 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     private val fetchTopThreeNotices: FetchTopThreeNotices,
     private val fetchTips: FetchTips
-) : BaseViewModel<HomeViewState, HomeEvent, HomeSideEffect>() {
+) : BaseViewModel<HomeViewState, HomeEvent, HomeSideEffect, HomeMutation>() {
     override fun setInitialState(): HomeViewState = HomeViewState()
 
     override fun handleEvent(event: HomeEvent) {
@@ -62,43 +66,82 @@ class HomeViewModel @Inject constructor(
         }
     }
 
+    // Side-Effect (Access Remote Repository)
     private fun getTopThreeNotices() = viewModelScope.launch {
         fetchTopThreeNotices()
             .fold(
                 onSuccess = { vo ->
-                    stateUpdate {
-                        it.copy(
-                            isLoading = false,
-                            isError = false,
-                            notificationGeneral = vo.general,
-                            notificationScholarship = vo.scholarship,
-                            notificationAcademic = vo.academic,
-                            notificationEvent = vo.event,
-                            notificationEmployment = vo.employment
-                        )
-                    }
+                    mutate(HomeMutation.MainContent.Success(vo))
                 },
                 onFailure = {
-                    stateUpdate {
-                        it.copy(
-                            isLoading = false,
-                            isError = true
-                        )
-                    }
+                    mutate(HomeMutation.MainContent.Failure(it.stackTraceToString()))
                 }
             )
     }
 
+    // Side Effect (Access Remote Repository)
     private fun getTips() = viewModelScope.launch {
         fetchTips()
             .collectLatest { result ->
-                result.onSuccess { vo ->
-                    stateUpdate {
-                        it.copy(
-                            tips = vo
-                        )
+                result.fold(
+                    onSuccess = { vo ->
+                        mutate(HomeMutation.Tip.Success(vo))
+                    },
+                    onFailure = {
+                        mutate(HomeMutation.Tip.Failure(it.stackTraceToString()))
                     }
+                )
+            }
+    }
+
+    // Main Reducer
+    override fun reduce(currentState: HomeViewState, mutation: HomeMutation): HomeViewState {
+        return when (mutation) {
+            is HomeMutation.MainContent -> currentState.copy(
+                mainContentState = mutation.reducer(currentState.mainContentState)
+            )
+            is HomeMutation.Tip -> currentState.copy(
+                tipState = mutation.reducer(currentState.tipState)
+            )
+        }
+    }
+
+    // Specialized Reducers
+    private fun HomeMutation.MainContent.reducer(state: MainContentState): MainContentState {
+        return when (this) {
+            is HomeMutation.MainContent.Loading -> state.copy(isLoading = true)
+            is HomeMutation.MainContent.Success -> state.copy(
+                isLoading = false,
+                isError = false,
+                notificationGeneral = topThreeNotices.general,
+                notificationAcademic = topThreeNotices.academic,
+                notificationScholarship = topThreeNotices.scholarship,
+                notificationEvent = topThreeNotices.event,
+                notificationEmployment = topThreeNotices.employment
+            )
+            is HomeMutation.MainContent.Failure -> {
+                state.copy(
+                    isLoading = false,
+                    isError = true
+                ).also {
+                    Log.d(this.javaClass.name, "Fetching Main Content Failed: ${reason}")
                 }
             }
+        }
+    }
+
+    private fun HomeMutation.Tip.reducer(state: TipState): TipState {
+        return when (this) {
+            is HomeMutation.Tip.Loading -> state.copy(isLoading = true)
+            is HomeMutation.Tip.Success -> state.copy(
+                isLoading = false,
+                isError = false,
+                tips = tips
+            )
+            is HomeMutation.Tip.Failure -> state.copy(
+                isLoading = false,
+                isError = true
+            )
+        }
     }
 }
