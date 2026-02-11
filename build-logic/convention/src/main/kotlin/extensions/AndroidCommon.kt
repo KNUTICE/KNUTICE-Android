8427664d93783cb20d3bff2c974c68cfef193179
Doyoon Kim <doyoon3312@kakao.com>
2026-02-11 00:23:50 -0500

    KAN-87 [REFACTOR] Apply Convention Plugin
      * Apply Convention Plugin for robust module management.
    
        - Force Jvm Version for Kotlin Compiler

--- build-logic/convention/src/main/kotlin/extensions/AndroidCommon.kt
+++ build-logic/convention/src/main/kotlin/extensions/AndroidCommon.kt
@@ -6,6 +6,9 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 // Project Extension for Android Common
 
@@ -26,6 +29,13 @@ internal fun Project.configureAndroidCommon() {
         with(compileOptions) {
             sourceCompatibility = JavaVersion.VERSION_17
             targetCompatibility = JavaVersion.VERSION_17
+        }
+
+        // Force Jvm Version for Kotlin Complier
+        tasks.withType<KotlinCompile>().configureEach {
+            compilerOptions {
+                jvmTarget.set(JvmTarget.JVM_17)
+            }
         }
 
         // Common Dependencies
