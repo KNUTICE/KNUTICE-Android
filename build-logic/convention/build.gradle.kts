66125a469c1a9b616a9bcfcc18614c323b5efb91
Doyoon Kim <doyoon3312@kakao.com>
2026-02-10 20:19:01 -0500

    KAN-87 [REFACTOR] Apply Convention Plugin
      * Apply Convention Plugin for robust module management.
    
        - Register ConventionPlugin
        - Include :build-logic to Root project's Gradle.

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,49 @@
+// Dependencies for configuring a custom plugin
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.doyoonkim.buildlogic"
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "knutice.android.application"
+            implementationClass = "plugins.AndroidApplicationConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "knutice.android.library"
+            implementationClass = "plugins.AndroidLibraryConventionPlugin"
+        }
+        register("androidCompose") {
+            id = "knutcie.android.compose"
+            implementationClass = "plugins.AndroidComposeConventionPlugin"
+        }
+        register("androidDagger") {
+            id = "knutice.android.dagger"
+            implementationClass = "plugins.AndroidDaggerConventionPlugin"
+        }
+        register("androidFirebase") {
+            id = "knutice.android.firebase"
+            implementationClass = "plugins.FirebaseConventionPlugin"
+        }
+        register("androidRoom") {
+            id = "knutice.android.room"
+            implementationClass = "plugins.AndroidRoomConventionPlugin"
+        }
+        register("jvmLibrary") {
+            id = "knutice.jvm.library"
+            implementationClass = "plugins.JvmLibraryConventionPlugin"
+        }
+    }
+}
+
+dependencies {
+    // Allow custom-defined plugins to refer Android/Kotlin classes.
+    // compileOnly --> Avoid bundle following libraries.
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.compose.compiler.plugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
