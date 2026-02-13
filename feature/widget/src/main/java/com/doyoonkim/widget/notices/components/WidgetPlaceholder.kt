bfe9466e876c9484665950472ae46fe50d494274
Doyoon Kim <doyoon3312@kakao.com>
2026-02-13 17:58:23 -0500

    KAN-57 [FEAT] Widget Feature Implementation   * Widget Feature implementation for feature consistency with iOS client
      - UI component Implementation
        * Reusable Composables
        * UI Utilities (State Validator, Intent Resolver)
        * Necessary Theme definition and resources, including Preview/Loading layout for Widget appearance in System Widget Picker.

---
+++ feature/widget/src/main/java/com/doyoonkim/widget/notices/components/WidgetPlaceholder.kt
@@ -0,0 +1,59 @@
+package com.doyoonkim.widget.notices.components
+
+import android.content.Intent
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
+import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
+import androidx.glance.appwidget.action.actionStartActivity
+import androidx.glance.appwidget.cornerRadius
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.Spacer
+import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.height
+import androidx.glance.layout.padding
+import androidx.glance.layout.wrapContentSize
+import androidx.glance.text.FontWeight
+import androidx.glance.text.Text
+import androidx.glance.text.TextAlign
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
+
+@Composable
+fun WidgetPlaceholder(
+    modifier: GlanceModifier = GlanceModifier,
+    titleText: String,
+    textColor: ColorProvider,
+    containerColor: ColorProvider,
+    extraContents: @Composable () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .cornerRadius(20.dp)
+            .background(containerColor),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = titleText,
+            style = TextStyle(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+                color = textColor,
+                textAlign = TextAlign.Center
+            ),
+            maxLines = 2,
+            modifier = GlanceModifier.fillMaxWidth().padding(15.dp)
+        )
+
+        Spacer(GlanceModifier.fillMaxWidth().height(5.dp))
+
+        // Extra Contents
+        extraContents()
+    }
+}
