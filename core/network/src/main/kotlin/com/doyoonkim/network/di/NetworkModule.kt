f12cf2e79eaea74d5c761609cef3038787e830d7
Doyoon Kim <doyoon3312@kakao.com>
2026-02-20 04:02:56 -0500

    [!FIX] Refactor Retrofit Instance Provider.
      - Identified Problem: Retrofit Instance managed by RetrofitProvider 'class' under Unscoped Dagger Dependency Graph causes re-instantiation for every single dependency injection, potentially causing massive Socket/ConnectionPool leaking.
      - Make Retrofit Provider as a Singleton object to ensure Dagger would reuse same instance of Retrofit instance.

--- core/network/src/main/kotlin/com/doyoonkim/network/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/doyoonkim/network/di/NetworkModule.kt
@@ -1,28 +1,15 @@
 package com.doyoonkim.network.di
 
-import com.doyoonkim.network.BuildConfig
 import com.doyoonkim.network.DeviceToken
 import com.doyoonkim.network.retrofit.KnuticeService
 import dagger.Module
 import dagger.Provides
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
 
 @Module(includes = [DeviceTokenModule::class])
 object NetworkModule {
-
     @Provides
-    fun providesKnuticeApi(provider: RetrofitProvider): KnuticeService {
-        return provider.provide().create(KnuticeService::class.java)
-    }
-}
-
-@Module(includes = [DeviceTokenModule::class])
-object NotificationNetworkModule {
-    @Provides
-    fun providesKnuticeApi(provider: RetrofitProvider): KnuticeService {
-        return provider.provide(5).create(KnuticeService::class.java)
+    fun providesKnuticeApi(): KnuticeService {
+        return RetrofitProvider.defaultRetrofit.create(KnuticeService::class.java)
     }
 }
 
--- core/network/src/main/kotlin/com/doyoonkim/network/di/RetrofitProvider.kt
+++ core/network/src/main/kotlin/com/doyoonkim/network/di/RetrofitProvider.kt
@@ -5,29 +5,33 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-import javax.inject.Inject
 
-class RetrofitProvider @Inject constructor() {
+object RetrofitProvider {
+    private val baseOkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                // User-Agent Interceptor
+                val req = chain.request()
+                    .newBuilder()
+                    .header("User-Agent", "android")
+                    .build()
+                chain.proceed(req)
+            }.build()
+    }
 
-    fun provide(timeout: Long = 10): Retrofit =
+    // Default API
+    internal val defaultRetrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BuildConfig.API_LIVE)
             .client(
-                OkHttpClient.Builder()
-                    .connectTimeout(timeout, TimeUnit.SECONDS)
-                    .readTimeout(timeout, TimeUnit.SECONDS)
-                    .writeTimeout(timeout, TimeUnit.SECONDS)
-                    .addInterceptor { chain ->
-                        // User-Agent Interceptor
-                        val req = chain.request()
-                            .newBuilder()
-                            .header("User-Agent", "android")
-                            .build()
-                        chain.proceed(req)
-                    }
+                // use .newBuilder() call to share ConnectionPool with baseOkHttpClient
+                baseOkHttpClient.newBuilder()
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(10, TimeUnit.SECONDS)
+                    .writeTimeout(10, TimeUnit.SECONDS)
                     .build()
             )
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-
+    }
 }
