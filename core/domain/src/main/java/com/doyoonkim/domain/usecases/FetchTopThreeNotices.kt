5c66aeadd1a5b6c227a8e3daef4ed380ad3dc867
Doyoon Kim <doyoon3312@kakao.com>
2026-03-11 22:28:54 -0400

    KAN-99 [REFACTOR] Implement Updated Widget Refresh Logic
      * Implement Pulling-based Widget Update Logic
        (Notice: 6 Hours + Manual Refresh)
        (Carrel: 30 minutes + Manual Refresh)
    
       - Modify Token Worker's existing work policy form UPDATE to KEEP to optimize periodic work via WorkManager (MainActivity)
       - Modify FetchTopThreeNotices usecase to asynchronously process multiple function call. (reduce loading time approx. 80%)
       - Provide comprehensive coroutine dispatcher (IO) to all function related to network call. (KnuticeRemoteSource)

--- core/domain/src/main/java/com/doyoonkim/domain/usecases/FetchTopThreeNotices.kt
+++ core/domain/src/main/java/com/doyoonkim/domain/usecases/FetchTopThreeNotices.kt
@@ -5,8 +5,8 @@ import com.doyoonkim.model.MajorCategory
 import com.doyoonkim.model.NoticeCategory
 import com.doyoonkim.model.NoticeVO
 import com.doyoonkim.model.TopThreeNoticeVO
-import com.doyoonkim.model.di.IoDispatcher
-import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import java.util.NoSuchElementException
 import javax.inject.Inject
 
@@ -17,19 +17,36 @@ interface FetchTopThreeNotices {
 
 class FetchTopThreeNoticesImpl @Inject constructor(
     private val remoteRepository: NoticeRemoteRepository,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : FetchTopThreeNotices {
 
     override suspend operator fun invoke(): Result<TopThreeNoticeVO> {
        return runCatching {
             remoteRepository.run {
-                TopThreeNoticeVO(
-                    general = queryTopThreeNotices(NoticeCategory.GENERAL_NEWS.name) ?: emptyList(),
-                    scholarship = queryTopThreeNotices(NoticeCategory.SCHOLARSHIP_NEWS.name) ?: emptyList(),
-                    event = queryTopThreeNotices(NoticeCategory.EVENT_NEWS.name) ?: emptyList(),
-                    academic = queryTopThreeNotices(NoticeCategory.ACADEMIC_NEWS.name) ?: emptyList(),
-                    employment = queryTopThreeNotices(NoticeCategory.EMPLOYMENT_NEWS.name) ?: emptyList()
-                )
+                coroutineScope {
+                    val general = async {
+                        queryTopThreeNotices(NoticeCategory.GENERAL_NEWS.name)
+                    }
+                    val scholarship = async {
+                        queryTopThreeNotices(NoticeCategory.SCHOLARSHIP_NEWS.name)
+                    }
+                    val academic = async {
+                        queryTopThreeNotices(NoticeCategory.ACADEMIC_NEWS.name)
+                    }
+                    val event = async {
+                        queryTopThreeNotices(NoticeCategory.EVENT_NEWS.name)
+                    }
+                    val employment = async {
+                        queryTopThreeNotices(NoticeCategory.EMPLOYMENT_NEWS.name)
+                    }
+
+                    TopThreeNoticeVO(
+                        general = general.await() ?: emptyList(),
+                        academic = academic.await() ?: emptyList(),
+                        scholarship = scholarship.await() ?: emptyList(),
+                        event = event.await() ?: emptyList(),
+                        employment = employment.await() ?: emptyList()
+                    )
+                }
             }
         }
     }
