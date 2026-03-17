c1220e47f8b00c3bb7c4f232275da657b4c4ef01
Doyoon Kim <doyoon3312@kakao.com>
2026-03-17 03:46:11 -0400

    KAN-99 [CHORE] Update Build Configuration
      - KNUTICE 1.7.0 Release Artifact
        * VersionCode: 37
        * VersionName = 1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ configure<ApplicationExtension>() {
 
     defaultConfig {
         applicationId = "com.doyoonkim.knutice"
-        versionCode = 36
+        versionCode = 37
         versionName = "@string/version_code"
     }
     buildTypes {
